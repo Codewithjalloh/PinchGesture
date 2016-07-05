5eb499d7d6a659a50fdce8be3e3c50b6cd078939
D.A Jalloh <info@codewithjalloh.com>
2016-07-05 23:59:36 +0100

    pinchGesture function added

--- PinchGesture/ViewController.swift
+++ PinchGesture/ViewController.swift
@@ -9,15 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var generalTzuImage: UIImageView!
+    var lastScaleFactor: CGFloat = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let pinchGesture:UIPinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: "pinchGesture:")
+        generalTzuImage.addGestureRecognizer(pinchGesture)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func pinchGesture(sender: UIPinchGestureRecognizer) {
+        let factor = sender
     }
 
 
